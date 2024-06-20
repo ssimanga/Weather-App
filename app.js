401ae17431050c3735ce0b3aa73f2b88363fff9f
Simanga Shabalala <simangae@gmail.com>
2024-06-20 09:10:23 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+
+    Directory: D:\Javascript ES6 Lessons\Weather App
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d----          2024/06/20    08:23                src
+
---
+++ src/app.js
@@ -0,0 +1,30 @@
+const weatherForm = document.querySelector('#weatherForm');
+const cityInput = document.querySelector('#cityInput');
+const weatherResult = document.querySelector('#waetherResult');
+
+weatherForm.addEventListener('submit', async(e)=>{
+    e.preventDefault();
+    const city = cityInput.value.trim();
+
+    if (city === '') return;
+
+    const apiKey = 'ba5993fc3ce596d6a1ceaff8a08d9861';
+    const url = `https://api.openweathermap.org/data/2.5/weather?=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+        const response = await fetch(url);
+        if(!response.ok) throw new Exrror('City not Found');
+
+        const data = await response.json();
+
+        const weatherHTML = `
+            <h2>${data.name}, ${data.sys.country}</h2>
+            <p><strong>Temparature:<strong>${data.main.temp} &deg;C</p>
+            <p><strong>Humidity:<strong>${data.main.humidity} %</p>
+            <p><strong>Conditions:<strong>${data.weather[0].description}</p>
+        `;
+        weatherResult.innerHTML = weatherHTML;
+    }catch(error){
+        weatherResult.innerHTML = `<div class="alert alert-danger">${error.message}</div>`;
+    }
+});
